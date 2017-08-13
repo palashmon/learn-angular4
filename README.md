feae1680c7f03f0458b06a53f7784d425627699a
Palash Mondal <palashm@live.in>
2017-08-13 18:04:53 +0530

    Looked into getting started with Angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LearnAngular4
+# Learn Angular4
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.6.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Learn Angular4';
 }
