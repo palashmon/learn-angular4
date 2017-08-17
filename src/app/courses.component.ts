be516be53cf07d45b61b45cdbd8b0cddbb7c9ba5
Palash Mondal <palashm@live.in>
2017-08-17 23:21:52 +0530

    Templates interpolation using function Syntax: {{ getTitle() }} where getTitle() function returns some string data

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  template: '<h3 class="subtitle">{{ title }}</h3>'
+  template: '<h3 class="subtitle">{{ getTitle() }}</h3>'
 })
 export class CoursesComponent {
   title = 'List of course';
+
+  getTitle() {
+    return `Here the title is: ${this.title}`;
+  }
 }
