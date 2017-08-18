d2233d0a830cf6f61106624fcfc73bff6fd5e63a
Palash Mondal <palashm@live.in>
2017-08-19 00:11:24 +0530

    Fixed an TS indentaion issue Mainly occuring inside the components

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-courses',
-  template: `
-    <h2 class="subtitle">{{ title }}</h2>
-    <ul>
-        <li *ngFor="let course of courses; let i=index">
-            {{i + 1}} - {{ course }}
-        </li>
-    </ul>
-  `
+    selector: 'app-courses',
+    template: `
+        <h2 class="subtitle">{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses; let i=index">
+                {{i + 1}} - {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
-  title = 'List of course';
+    title = 'List of course';
 
-  // Here we have an static array for demo
-  courses = ['Course 1', 'Course 2', 'Course 3'];
+    // Here we have an static array for demo
+    courses = ['Course 1', 'Course 2', 'Course 3'];
 }
