cbb0ce50fbe59be7bf3ad8dc86470666dc00904d
Palash Mondal <palashm@live.in>
2017-08-18 21:50:47 +0530

    Add directive sample code Looked into structural directives. More info: https://angular.io/guide/structural-directives Syntax: *ngFor More info: https://angular.io/guide/template-syntax#ngFor

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  template: '<h3 class="subtitle">{{ getTitle() }}</h3>'
+  template: `
+    <h2 class="subtitle">{{ title }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{ course }}
+        </li>
+    </ul>
+  `
 })
 export class CoursesComponent {
   title = 'List of course';
 
-  getTitle() {
-    return `Here the title is: ${this.title}`;
-  }
+  // Here we have an static array for demo
+  courses = ['Course 1', 'Course 2', 'Course 3'];
 }
