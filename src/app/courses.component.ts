02492f5fba86029579382b94d385d7c41f26fc0d
Palash Mondal <palashm@live.in>
2017-08-20 12:25:38 +0530

    Bind multiple class using class binding Syntax: [ngClass]='moreClasses'

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,11 +5,19 @@ import { Component } from '@angular/core';
     template: `
         <p class="lead">{{ title }}</p>
         <p>
-            <button type="button" class="btn btn-primary" [class.active]="isActive">Primary</button>
+            <button type="button" class="btn" [ngClass]="moreClasses">Success</button>
         </p>
     `
 })
 export class CoursesComponent {
     title = 'List of course';
+
     isActive = true;
+    success = true;
+
+    // moreClasses is a property in the component that contains an object
+    moreClasses = {
+        'btn-success': this.success,
+        active: this.isActive
+    };
 }
