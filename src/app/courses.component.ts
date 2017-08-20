3eb499b82f248b4f3c85edfe5c6f6ff8dd2e7107
Palash Mondal <palashm@live.in>
2017-08-20 12:44:13 +0530

    Bind single styling using style binding Syntax: [style.property-name]='expression'

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,19 +5,11 @@ import { Component } from '@angular/core';
     template: `
         <p class="lead">{{ title }}</p>
         <p>
-            <button type="button" class="btn" [ngClass]="getClasses()">Primary</button>
+            <button [style.fontSize.px]="isImportant ? '30' : '16'">Primary</button>
         </p>
     `
 })
 export class CoursesComponent {
     title = 'List of course';
-
-    isActive = true;
-    isPrimary = true;
-
-    // moreClasses is a property in the component that contains an object
-    getClasses() {
-        const moreClasses = { 'btn-primary': this.isPrimary, active: this.isActive };
-        return moreClasses;
-    }
+    isImportant = true;
 }
