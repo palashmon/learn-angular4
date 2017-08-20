d0c63aae6348bec45cd3a9d9e0a4685492e00a17
Palash Mondal <palashm@live.in>
2017-08-20 14:07:57 +0530

    Added Event binding code

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,22 +4,13 @@ import { Component } from '@angular/core';
     selector: 'app-courses',
     template: `
         <p class="lead">{{ title }}</p>
-        <p>
-            <button [ngStyle]="getStyles()">Primary</button>
-        </p>
+        <button (click)="onSave($event)" class="btn btn-primary">Save</button>
     `
 })
 export class CoursesComponent {
-    title = 'List of course';
-    isImportant = true;
-
-    // moreStyles is a property in the component that contains an object with css property names as the keys
-    getStyles() {
-        const moreStyles = {
-            'background-color': '#369',
-            'font-size': '16px',
-            color: 'white'
-        };
-        return moreStyles;
+    title = 'Event Binding';
+    onSave($event) {
+        console.log($event);
+        console.log('Button was clicked!');
     }
 }
