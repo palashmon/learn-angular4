9feb34407399ea3428574e0f89cfae781c57cd68
Palash Mondal <palashm@live.in>
2017-08-20 12:10:21 +0530

    Bind a single class using class binding Syntax: [class.active]='isActive'

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,25 +5,11 @@ import { Component } from '@angular/core';
     template: `
         <p class="lead">{{ title }}</p>
         <p>
-            <button type="button" class="btn">Basic</button>
-            <button type="button" class="btn btn-default">Default</button>
-            <button type="button" class="btn btn-primary">Primary</button>
-            <button type="button" class="btn btn-success">Success</button>
-            <button type="button" class="btn btn-info">Info</button>
-            <button type="button" class="btn btn-warning">Warning</button>
-            <button type="button" class="btn btn-danger">Danger</button>
-            <button type="button" class="btn btn-link">Link</button>
+            <button type="button" class="btn btn-primary" [class.active]="isActive">Primary</button>
         </p>
     `
 })
 export class CoursesComponent {
     title = 'List of course';
-    dtsst = ['hey', 'ola'];
-
-    // Here we have an static array for demo
-    // courses;
-
-    // constructor(service: CoursesService) {
-    //     this.courses = service.getCourses();
-    // }
+    isActive = true;
 }
