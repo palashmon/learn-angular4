fc68f8d812c413638ec29fd46d521da0a37fae29
Palash Mondal <palashm@live.in>
2017-08-21 20:36:19 +0530

    Checking if enter key was pressed, angular way Syntax: (keyup.enter)='onKeyUp()' Less code and much cleaner.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,14 +4,12 @@ import { Component } from '@angular/core';
     selector: 'app-courses',
     template: `
         <p class="lead">{{ title }}</p>
-        <input (keyup)="onKeyUp($event)" />
+        <input (keyup.enter)="onKeyUp()" />
     `
 })
 export class CoursesComponent {
     title = 'Event Filtering';
-    onKeyUp($event) {
-        if ($event.keyCode === 13) {
-            console.log('Enter was pressed!');
-        }
+    onKeyUp() {
+        console.log('Enter was pressed!');
     }
 }
