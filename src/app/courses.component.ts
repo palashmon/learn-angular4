9d6be618c46e064d008206f12ea9b45d7e21e060
Palash Mondal <palashm@live.in>
2017-08-22 20:43:14 +0530

    Add built-in pipes code Syntax: {{ course.title | uppercase }}

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,14 +3,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-courses',
     template: `
-        <p>{{ username }}</p>
-        <input [(ngModel)]="username" (keyup.enter)="onKeyUp()" />
+        {{ course.title | uppercase }} <br/>
+        {{ course.students | number }} <br/>
+        {{ course.rating | number:'1.2-2' }} <br/>
+        {{ course.price | currency:'USD':true:'3.2-2' }} <br/>
+        {{ course.releaseDate | date:'shortDate'}}
     `
 })
 export class CoursesComponent {
-    username = 'wes';
-
-    onKeyUp(username) {
-        console.log(this.username);
-    }
+    course = {
+        title: 'The Angular Crash Course',
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2017, 8, 21)
+    };
 }
