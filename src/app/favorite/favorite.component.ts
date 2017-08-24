df9f5d95bf19992b72e3f5333277d27dc02566e4
Palash Mondal <palashm@live.in>
2017-08-24 20:46:07 +0530

    Add code for data-bound input property Syntax: @Input() isFavorite Html: <app-favorite [isFavorite]='post.isFavorite'></app-favorite>

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-favorite',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-    isFavorite: boolean;
+    // Declare a data-bound input property
+    @Input() isFavorite: boolean;
 
     constructor() {}
 
