3572aa97cc8c8456feae3a5db41d90d5894682d3
Palash Mondal <palashm@live.in>
2017-09-14 21:21:38 +0530

    Added template driven form with ngModel

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
    templateUrl: './contact-form.component.html',
    styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-   constructor() {}
-
-   ngOnInit() {}
+export class ContactFormComponent {
+   firstName: string;
+   comment: string;
 }
