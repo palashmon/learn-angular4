25b187509a0e41f238139a61ecdc98555892634d
Palash Mondal <palashm@live.in>
2017-09-14 20:12:41 +0530

    Updated input-format directive

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,21 +1,22 @@
 import { Directive, Input, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-    selector: '[appInputFormat]'
+   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-    @Input('appInputFormat') format;
-    constructor(private el: ElementRef) {}
+   // tslint:disable-next-line:no-input-rename
+   @Input('appInputFormat') format;
+   constructor(private el: ElementRef) {}
 
-    @HostListener('blur')
-    onblur() {
-        let value: string = this.el.nativeElement.value;
+   @HostListener('blur')
+   onblur() {
+      const value: string = this.el.nativeElement.value;
 
-        // Check the format supplied from the html host
-        if (this.format === 'lowercase') {
-            this.el.nativeElement.value = value.toLowerCase();
-        } else {
-            this.el.nativeElement.value = value.toUpperCase();
-        }
-    }
+      // Check the format supplied from the html host
+      if (this.format === 'lowercase') {
+         this.el.nativeElement.value = value.toLowerCase();
+      } else {
+         this.el.nativeElement.value = value.toUpperCase();
+      }
+   }
 }
