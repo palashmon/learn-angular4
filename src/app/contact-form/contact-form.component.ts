1c9b7089b60f57ac2d9f2bc1aacf2182dde4db74
Palash Mondal <palashm@live.in>
2017-09-26 11:31:51 +0530

    Learned about ngForm use case

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-   // tslint:disable-next-line:component-selector
-   selector: 'contact-form',
-   templateUrl: './contact-form.component.html',
-   styleUrls: ['./contact-form.component.css']
+    // tslint:disable-next-line:component-selector
+    selector: 'contact-form',
+    templateUrl: './contact-form.component.html',
+    styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
-   firstName: string;
-   comment: string;
+    firstName: string;
+    comment: string;
+    submit(myForm) {
+        // If the form is valid then only show the log
+        if (myForm.valid) {
+            console.log(myForm);
+        }
+    }
 }
